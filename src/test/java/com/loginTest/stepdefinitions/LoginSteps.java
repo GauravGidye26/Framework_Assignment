f487865388e1796b17b32af1f9730eae0ab3b86e
GauravGidye26 <gauravgidye26@gmail.com>
2024-09-18 17:48:31 +0530

    Initial commit

---
+++ src/test/java/com/loginTest/stepdefinitions/LoginSteps.java
@@ -0,0 +1,52 @@
+package com.loginTest.stepdefinitions;
+
+import com.loginTest.pages.LoginPage;
+import com.loginTest.utils.DriverFactory;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class LoginSteps extends DriverFactory {
+    WebDriver driver = new ChromeDriver();
+    LoginPage loginPage = new LoginPage(driver);
+
+    @Given("I am on the login page")
+    public void i_am_on_the_login_page() {
+        loginPage.openLoginPage();
+    }
+
+    @When("I enter valid credentials")
+    public void i_enter_valid_credentials() {
+        loginPage.enterEmail("hardstop-pharmacist6@mailinator.com");
+        loginPage.enterPassword("Password1#");
+        loginPage.clickSignIn();
+    }
+
+    @When("I enter an invalid password")
+    public void i_enter_an_invalid_password() {
+        loginPage.enterEmail("hardstop-pharmacist6@mailinator.com");
+        loginPage.enterPassword("Password");
+        loginPage.clickSignIn();
+    }
+
+    @When("I enter an invalid email")
+    public void i_enter_an_invalid_email() {
+        loginPage.enterEmail("hardstop-pharmacist6@mailinator.co");
+        loginPage.enterPassword("Password1#");
+        loginPage.clickSignIn();
+    }
+
+    @Then("I should be redirected to the dashboard")
+    public void i_should_be_redirected_to_the_dashboard() {
+        Assert.assertTrue(loginPage.isDashboardDisplayed(), "Dashboard is not displayed");
+    }
+
+    @Then("I should see an error message {string}")
+    public void i_should_see_an_error_message(String expectedMessage) {
+        String actualMessage = loginPage.getErrorMessage();
+        Assert.assertEquals(actualMessage, expectedMessage, "Error message does not match");
+    }
+}

4ef30da97bf5ae1c6b6055389ac0f25e665b5795
GauravGidye26 <110521099+GauravGidye26@users.noreply.github.com>
2024-09-18 18:33:03 +0530

    Initial commit

--- src/test/java/com/loginTest/stepdefinitions/LoginSteps.java
+++ src/test/java/com/loginTest/stepdefinitions/LoginSteps.java
@@ -13,39 +13,39 @@ public class LoginSteps extends DriverFactory {
     WebDriver driver = new ChromeDriver();
     LoginPage loginPage = new LoginPage(driver);
 
-    @Given("I am on the login page")
-    public void i_am_on_the_login_page() {
+    @Given("User the login page")
+    public void user_on_the_login_page() {
         loginPage.openLoginPage();
     }
 
-    @When("I enter valid credentials")
-    public void i_enter_valid_credentials() {
+    @When("User enter valid credentials")
+    public void user_enter_valid_credentials() {
         loginPage.enterEmail("hardstop-pharmacist6@mailinator.com");
         loginPage.enterPassword("Password1#");
         loginPage.clickSignIn();
     }
 
-    @When("I enter an invalid password")
-    public void i_enter_an_invalid_password() {
+    @When("User enter an invalid password")
+    public void user_enter_an_invalid_password() {
         loginPage.enterEmail("hardstop-pharmacist6@mailinator.com");
         loginPage.enterPassword("Password");
         loginPage.clickSignIn();
     }
 
-    @When("I enter an invalid email")
-    public void i_enter_an_invalid_email() {
+    @When("User enter an invalid email")
+    public void user_enter_an_invalid_email() {
         loginPage.enterEmail("hardstop-pharmacist6@mailinator.co");
         loginPage.enterPassword("Password1#");
         loginPage.clickSignIn();
     }
 
-    @Then("I should be redirected to the dashboard")
-    public void i_should_be_redirected_to_the_dashboard() {
+    @Then("User should be redirected to the dashboard")
+    public void user_should_be_redirected_to_the_dashboard() {
         Assert.assertTrue(loginPage.isDashboardDisplayed(), "Dashboard is not displayed");
     }
 
-    @Then("I should see an error message {string}")
-    public void i_should_see_an_error_message(String expectedMessage) {
+    @Then("User should see an error message {string}")
+    public void user_should_see_an_error_message(String expectedMessage) {
         String actualMessage = loginPage.getErrorMessage();
         Assert.assertEquals(actualMessage, expectedMessage, "Error message does not match");
     }

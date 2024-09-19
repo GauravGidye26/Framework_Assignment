8f60bea2b605da09514c478d25cf45de9195c182
GauravGidye26 <gauravgidye26@gmail.com>
2024-09-19 12:47:23 +0530

    Used Regular Expressions

--- src/test/java/com/loginTest/stepdefinitions/LoginSteps.java
+++ src/test/java/com/loginTest/stepdefinitions/LoginSteps.java
@@ -13,38 +13,24 @@ public class LoginSteps extends DriverFactory {
     WebDriver driver = new ChromeDriver();
     LoginPage loginPage = new LoginPage(driver);
 
-    @Given("User on the login page")
+    @Given("^User on the login page$")
     public void user_on_the_login_page() {
         loginPage.openLoginPage();
     }
 
-    @When("User enter valid credentials")
-    public void user_enter_valid_credentials() {
-        loginPage.enterEmail("hardstop-pharmacist6@mailinator.com");
-        loginPage.enterPassword("Password1#");
+    @When("^User login with \"([^\"]*)\" and \"([^\"]*)\"$")
+    public void user_login_with_credentials(String email, String password) {
+        loginPage.enterEmail(email);
+        loginPage.enterPassword(password);
         loginPage.clickSignIn();
     }
 
-    @When("User enter an invalid password")
-    public void user_enter_an_invalid_password() {
-        loginPage.enterEmail("hardstop-pharmacist6@mailinator.com");
-        loginPage.enterPassword("Password");
-        loginPage.clickSignIn();
-    }
-
-    @When("User enter an invalid email")
-    public void user_enter_an_invalid_email() {
-        loginPage.enterEmail("hardstop-pharmacist6@mailinator.co");
-        loginPage.enterPassword("Password1#");
-        loginPage.clickSignIn();
-    }
-
-    @Then("User should be redirected to the dashboard")
+    @Then("^User should be redirected to the dashboard$")
     public void user_should_be_redirected_to_the_dashboard() {
         Assert.assertTrue(loginPage.isDashboardDisplayed(), "Dashboard is not displayed");
     }
 
-    @Then("User should see an error message {string}")
+    @Then("^User should see an error message \"([^\"]*)\"$")
     public void user_should_see_an_error_message(String expectedMessage) {
         String actualMessage = loginPage.getErrorMessage();
         Assert.assertEquals(actualMessage, expectedMessage, "Error message does not match");
